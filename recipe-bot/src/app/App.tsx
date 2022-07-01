d5ecc386971b8aecc0a1a2bda86519454d264dce
cjaquith <cjaquith98@gmail.com>
2022-07-01 10:28:21 -0400

    initial commit - cleaned up

---
+++ recipe-bot/src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box } from "@mui/material";
+import HomeTab from "../components/tabs/HomeTab";
+
+function App() {
+  return (
+    <Box>
+      <HomeTab />
+    </Box>
+  );
+}
+
+export default App;
---
+++ recipe-bot/src/components/tabs/HomeTab.tsx
@@ -0,0 +1,15 @@
+import { Box, Paper } from "@mui/material";
+
+export default function HomeTab() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        verticalAlign: "middle",
+        alignItems: "center",
+      }}
+    >
+      <Paper></Paper>
+    </Box>
+  );
+}
